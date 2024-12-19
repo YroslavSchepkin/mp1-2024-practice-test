8e93aa35c158859e10591c6ea800491da7d380bb
YroslavSchepkin <zvezdny153@gmail.com>
2024-12-19 09:54:07 +0300

    Update task2.c

--- 3824Б1ПР2/28 Щепкин Ярослав Андреевич/task2.c
+++ 3824Б1ПР2/28 Щепкин Ярослав Андреевич/task2.c
@@ -1,8 +1,26 @@
-// Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
-// Найти значение максимального из его локальных максимумов.
-// Локальный максимум – элемент, который >= левого и правого соседей.
-// При отсутствии локальных максимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+#include <stdio.h>
+#include <locale.h>
+long long task2(unsigned A[], size_t n) {
+    if (n < 3) {
+        return -1; 
+    }
+    long long max = -1; 
+    for (size_t i = 1; i < n - 1; ++i) { 
+        if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) { 
+            if (A[i] > max) {
+                max = A[i]; 
+            }
+        }
+    }
+    return (max == -1) ? -1 : max; 
+}
+int main() {
+    setlocale(LC_ALL, "rus");
+    unsigned A[] = { 1, 3, 2, 4, 1, 5 };
+    size_t n = sizeof(A) / sizeof(A[0]);
+
+    long long result = task2(A, n);
+    printf("максимум: %lld\n", result);
+
+    return 0;
 }
