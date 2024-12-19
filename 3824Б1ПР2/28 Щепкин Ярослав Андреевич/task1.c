55fae68cd46d7850f25f4877db40d30f5c49d954
YroslavSchepkin <zvezdny153@gmail.com>
2024-12-19 09:24:14 +0300

    Update task1.c

--- 3824Б1ПР2/28 Щепкин Ярослав Андреевич/task1.c
+++ 3824Б1ПР2/28 Щепкин Ярослав Андреевич/task1.c
@@ -1,7 +1,34 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел, среди которых
 // не менее двух нулей. Найти сумму элементов, расположенных между последними 
 // двумя нулями.
-int task1(int A[], size_t n)
-{
-  return -1111111111;
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    int A[] = { 1, 0, 2, 3, 0, 5, 5, 0, 7 };
+    int n = sizeof(A) / sizeof(A[0]);
+
+    int lastZero = -1;
+    int secondLast = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (A[i] == 0) {
+            secondLast = lastZero;
+            lastZero = i;
+        }
+    }
+
+    if (secondLast != -1 && lastZero != -1) {
+        int sum = 0;
+        for (int i = secondLast + 1; i < lastZero; i++) {
+            sum += A[i];
+        }
+        printf("Сумма элементов последних двух нулей: %d\n", sum);
+    }
+    else {
+        printf("мало нулей в массиве.\n");
+    }
+    return 0;
 }
